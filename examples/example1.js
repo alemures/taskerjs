3f5c20d15fb34d72bb09e32488882cb53614867f
alejandro <alemures@hotmail.com>
2015-08-14 18:45:03 +0100

    Using jscs, jshint and mocha as described in the repo nodejs-project-example in my github

---
+++ examples/example1.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var taskerjs = require('../index');
+
+var acc = new taskerjs.Accumulator({
+  tasksLimit: 500, // Limit of tasks to call to the worker
+  timeLimit: 1000, // Elapsed time between calls to the worker in milliseconds
+  maxTasksLimit: 800 // Max number of tasks by worker execution
+});
+
+acc.setWorker(function(tasks, cb) {
+  console.log(tasks.length + ' tasks processed');
+  cb();
+});
+
+for (var i = 0; i < 111100; i++) {
+  acc.add(createQuery());
+}
+
+setInterval(function() {
+  for (var i = 0; i < 2600; i++) {
+    acc.add(createQuery());
+  }
+}, 3000);
+
+function createQuery() {
+  return 'select * from user where name like "A%" and age > 18 order by name limit 100;';
+}
--- test/test.js
+++ test/test.js
@@ -1,30 +1,11 @@
-var taskerjs = require('../index');
+'use strict';
 
-var acc = new taskerjs.Accumulator({
-    tasksLimit: 500, // Limit of tasks to call to the worker
-    timeLimit: 1000, // Elapsed time between calls to the worker in milliseconds
-    maxTasksLimit: 800 // Max number of tasks by worker execution
+var describe = require('mocha').describe;
+var it = require('mocha').it;
+var chai = require('chai');
+
+describe('test', function() {
+  it('should be true', function() {
+    chai.assert(true, 'true');
+  });
 });
-
-acc.setWorker(function(tasks, cb) {
-    console.log(tasks.length + ' tasks processed');
-    cb();
-});
-
-for(var i = 0; i < 111100; i++) {
-    acc.add(createQuery());
-}
-
-setInterval(function() {
-   for(var i = 0; i < 2600; i++) {
-       acc.add(createQuery());
-   }
-}, 3000);
-
-function createQuery() {
-    return 'select * from user where name like "A%" and age > 18 order by name limit 100;';
-}
-
-function randomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-}
